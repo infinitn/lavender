30fa3add478b1108552dfdc044e9f6d5c119779d
work <840181150@qq.com>
2017-07-04 20:57:02 +0800

    解决遗留问题，创建文件基本结构，正式开始开发阶段

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,36 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-// router.get('./page1',function (req,res,next) {
-//    res.render('./page1') ;
-// });
+
+router.get('/about', function(req, res, next) {
+    res.render('./about',{
+        message:req.query.message
+    });
+});
+router.get('/activity', function(req, res, next) {
+    res.render('./activity',{
+        message:req.query.message
+    });
+});
+router.get('/play', function(req, res, next) {
+    res.render('./play',{
+        message:req.query.message
+    });
+});
+router.get('/eat', function(req, res, next) {
+    res.render('./eat',{
+        message:req.query.message
+    });
+});
+router.get('/reside', function(req, res, next) {
+    res.render('./reside',{
+        message:req.query.message
+    });
+});
+router.get('/map', function(req, res, next) {
+    res.render('./map',{
+        message:req.query.message
+    });
+});
 //待处理  注册路由
 module.exports = router;
